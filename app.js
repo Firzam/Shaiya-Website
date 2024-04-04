3ecd26ac3d43ee5e750b25b86d5e915c3283142f
Grégory Mk <firzamdigidix@gmail.com>
2024-04-05 01:22:44 +0200

    Feat: Ease things up and use public repo as archive

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const fs = require('fs')
 
 const accountRoutes = require('./routes/account');
 const updaterRoutes = require('./routes/updater');
